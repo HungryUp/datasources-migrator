ca3a33d870f22aff2e8632483033455b87e70007
Gaël <gael@hungryup.co>
2017-09-22 17:23:00 +0200

    🐛 Removing console calls + some style fixes

--- bin.js
+++ bin.js
@@ -5,7 +5,7 @@ const { join } = require('path');
 const packageConfig = require('./package.json');
 
 function loadConfig(location) {
-  if (process && typeof location !== "object") {
+  if (process && typeof location !== 'object') {
     return require(join(process.cwd(), location));
   }
   return location;
@@ -16,13 +16,12 @@ program
   .option('-c, --config <config>', 'Configuration file, default ./seeds.json', loadConfig, null)
   .option('-R, --rollback', 'Rollback migrations')
   .arguments('[datasources...]')
-  .parse(process.argv)
+  .parse(process.argv);
 
 if (!program.config) {
   program.config = require(join(process.cwd(), 'migrations.json'));
 }
-console.log('call');
 
 module.exports = migrator(program, process.cwd())
-  .then(r => process.exit(0))
-  .catch(error => { throw error });
+  .then(() => process.exit(0))
+  .catch((error) => { throw error; });
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { promisify } = require('util');
 const fs = require('fs');
 const { join } = require('path');
+
 const readdir = promisify(fs.readdir);
 
-module.exports = async ({config, args, rollback}, workingDir) => {
-  const sources = (args.length > 0 ? args : Object.keys(config.sources))
+module.exports = async ({ config = {}, args = [], rollback }, workingDir) => {
+  const sources = (args.length > 0 ? args : Object.keys(config.sources || {}))
     .reduce((obj, key) => {
       obj[key] = config.sources[key] || {};
       return obj;
@@ -15,9 +16,11 @@ module.exports = async ({config, args, rollback}, workingDir) => {
   const migratoring = [];
   for (const sourceName of sourceNames) {
     migratoring.push((async () => {
-      const source =  Object.assign({ rollback }, sources[sourceName]);
+      const source = Object.assign({ rollback }, sources[sourceName]);
       const files = await readdir(join(workingDir, source.path));
-      const { play, init, end } = require(join(workingDir, 'node_modules', source.driver))(source, workingDir);
+      const driverPath = join(workingDir, 'node_modules', source.driver);
+      const driver = require(driverPath);
+      const { play, init, end } = driver(source, workingDir);
       const db = await init();
       await play(db, files);
       await end(db);
