1b6f1343f973ea0f724c1db7fe797592ec5c8260
Gaël <gael@hungryup.co>
2017-09-22 12:51:01 +0200

    ✨ Split module & bin + refactor config

---
+++ bin.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const program = require('commander');
+const migrator = require('./index.js');
+const { join } = require('path');
+const packageConfig = require('./package.json');
+
+function loadConfig(location) {
+  if (process && typeof location !== "object") {
+    return require(join(process.cwd(), location));
+  }
+  return location;
+}
+
+program
+  .version(packageConfig.version)
+  .option('-c, --config <config>', 'Configuration file, default ./seeds.json', loadConfig, null)
+  .option('-R, --rollback', 'Rollback migrations')
+  .arguments('[datasources...]')
+  .parse(process.argv)
+
+if (!program.config) {
+  program.config = require(join(process.cwd(), 'migrations.json'));
+}
+console.log('call');
+
+module.exports = migrator(program, process.cwd())
+  .then(r => process.exit(0))
+  .catch(error => { throw error });
--- index.js
+++ index.js
@@ -1,52 +1,29 @@
-#!/usr/bin/env node
-const program = require('commander');
 const { promisify } = require('util');
 const fs = require('fs');
 const { join } = require('path');
-const packageConfig = require('./package.json');
 const readdir = promisify(fs.readdir);
 
-function loadConfig(location) {
-  if (process && typeof location !== "object") {
-    return require(join(process.cwd(), location));
+module.exports = async ({config, args, rollback}, workingDir) => {
+  const sources = (args.length > 0 ? args : Object.keys(config.sources))
+    .reduce((obj, key) => {
+      obj[key] = config.sources[key] || {};
+      return obj;
+    }, {});
+
+
+  const sourceNames = Object.keys(sources);
+  const migratoring = [];
+  for (const sourceName of sourceNames) {
+    migratoring.push((async () => {
+      const source =  Object.assign({ rollback }, sources[sourceName]);
+      const files = await readdir(join(workingDir, source.path));
+      const { play, init, end } = require(join(workingDir, 'node_modules', source.driver))(source, workingDir);
+      const db = await init();
+      await play(db, files);
+      await end(db);
+    })());
   }
-  return location;
-}
 
-program
-  .version(packageConfig.version)
-  .option('-c, --config <config>', 'Configuration file, default ./seeds.json', loadConfig, null)
-  .option('-R, --rollback', 'Rollback migrations')
-  .arguments('[datasources...]')
-  .parse(process.argv)
-
-if (!program.config) {
-  program.config = require(join(process.cwd(), 'migrations.json'));
-}
-
-const { config, args, rollback } = program;
-const sources = (args.length > 0 ? args : Object.keys(config.sources))
-  .reduce((obj, key) => {
-    obj[key] = config.sources[key] || {};
-    return obj;
-  }, {});
-
-
-const sourceNames = Object.keys(sources);
-const migratoring = [];
-for (const sourceName of sourceNames) {
-  migratoring.push((async () => {
-    const source =  Object.assign({ rollback }, sources[sourceName]);
-    const files = await readdir(join(process.cwd(), source.path));
-    const { play, init, end } = require(join(process.cwd(), 'node_modules', source.driver));
-    const db = await init(source);
-    await play(db, source, files);
-    await end(db, source);
-  })());
-}
-
-Promise
-  .all(migratoring)
-  .then(r => process.exit(0))
-  .catch(error => { throw error });
-
+  return Promise
+    .all(migratoring);
+};
